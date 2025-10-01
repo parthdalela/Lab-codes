4df7891dc26780ebde8199adc20883bff97eaaad
parthdalela <parthdalela@gmail.com>
2025-10-01 10:10:19 +0530

    Add files via upload

---
+++ lab codes/namenumber.c
@@ -0,0 +1,19 @@
+// programme to input the name and number of user
+#include <stdio.h>
+int main()
+{ char name[5];int number[10];
+  printf("Enter your name = "); 
+  scanf("%s",&name);
+  for(int i=0;i<=10;i++)
+  {
+    printf("Enter your phone number digit = ");
+    scanf("%d",&number[i]);
+  }
+  printf("Name of the user is = %s \n",name);
+ for(int i=0;i,10;i++)
+ {
+    printf(i);
+ }
+  return 0;
+}
+
